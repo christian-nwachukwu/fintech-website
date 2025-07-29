449af995832ab7aaac7bc17b1debde9c9bcec6b1
Christian Nwachukwu <christian.nwachukwu2020@gmail.com>
2025-07-29 04:23:51 +0100

    add info.json, Dashboard,Main & there css files - //Update Main

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './main.css'
 import PageTitle from './PageTitle'
+import Dashboard from './Dashboard'
 
 const Main = () => {
   return (
     <main id='main' className='main'>
         <PageTitle page="Dashboard" />
+        <Dashboard />
     </main>
   )
 }
